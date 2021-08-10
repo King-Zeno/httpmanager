381ff1217a9f8bf1e7e6e8b3bc504caaa127f5a5
kuangzhirong <953532127@qq.com>
2021-08-10 14:17:13 +0800

    add report

---
+++ manager/views/report.py
@@ -0,0 +1,19 @@
+from rest_framework.response import Response
+from utils.common import CustomViewBase, JsonResponse
+from rest_framework.decorators import action
+from manager.models.report import Report
+from manager.serializers.report import ReportSerializer, ReportListSerializer
+
+
+class ReportViewSet(CustomViewBase):
+    queryset = Report.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return ReportListSerializer
+        return ReportSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        object = self.get_object()
+        object.delete()
+        return JsonResponse(code=200, msg="删除成功")
--- urls/manager.py
+++ urls/manager.py
@@ -3,12 +3,13 @@ from django.urls import path, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework_extensions.routers import ExtendedDefaultRouter, ExtendedSimpleRouter
 
-from manager.views import project, env, api, plan, case
+from manager.views import project, env, api, plan, case, report
 
 router = ExtendedDefaultRouter()
 project_router = router.register(r'project', project.ProjectViewSet, basename='project')
 project_router.register(r'env', project.ProjectEnvViewSet, basename='project-env', parents_query_lookups=['project'])
 project_router.register('api', api.ApiViewSet, basename='project-api',parents_query_lookups=['project'])
+project_router.register('report', report.ReportViewSet, basename='project-report', parents_query_lookups=['project'])
 project_router.register(r'plans',
                         plan.PlanParamViewSet,
                         basename='project-plan',
